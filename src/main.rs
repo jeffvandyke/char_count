48e396b758a49ed5bb184cb83962d5517144083e
Jeff VanDyke <jeffvd.a216@gmail.com>
2018-04-18 23:07:35 -0400

    5-line display/scrollback basically working, WIP, slow on line change

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,11 @@ use pancurses as curses;
 
 fn main() {
     let window = curses::initscr();
-    window.printw("Done initializing");
     curses::noecho();
 
     main_loop(&window);
 
     curses::endwin();
-    println!("All done!")
 }
 
 #[derive(Debug,Default)]
@@ -36,20 +34,46 @@ impl CharCounts {
     }
 }
 
+/// Writes the entire state of the window out to the screen.
+fn render(wnd: &curses::Window, text: &String, counts: &CharCounts)
+{
+    // Shoot, I don't know how to use curses best!
+    let screen_width = wnd.get_max_x();
+
+    // first, print out 1st 5 lines of text
+    const MAX_ROWS: i32 = 5;
+    let str_n_rows
+        = ((text.len() as f32) / (screen_width as f32)).ceil() as i32;
+
+    // just try a little functional weirdness and hope I get this math right
+    let text_row_width = screen_width - 2;
+    let text_print_rows = (std::cmp::max(str_n_rows - MAX_ROWS, 0)..str_n_rows)
+        .map(|row| text.get(
+                (row * text_row_width) as usize
+                ..std::cmp::min(
+                    (row + 1) * text_row_width,
+                    text.len() as i32) as usize));
+
+    wnd.mv(0, 0);
+    for print_row_n in text_print_rows {
+        wnd.printw("| ");
+        wnd.printw(print_row_n.unwrap_or("HELP! BAD INDEX!!!"));
+    }
+}
 
 fn main_loop(window: &curses::Window) {
     let mut counts: CharCounts = Default::default();
+    let mut text = String::new();
 
-    println!("(can i even do this?)");
     loop {
-        let ch: char = match window.getch() {
+        render(&window, &text, &counts);
+
+        match window.getch() {
             Some(curses::Input::Character(input)) => {
+                text.push(input);
                 counts.add_char(input);
-                input
             },
             _ => break
         };
-
-        window.printw(&ch.to_string());
     }
 }
