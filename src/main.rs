fa6f69407b91b6ca6a393ee229f902d8aa7b1b8d
Jeff VanDyke <jeffvd.a216@gmail.com>
2019-06-03 23:07:31 -0400

    Add clippy and fix, remove outdated render function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+#![deny(clippy::all)]
+#![deny(clippy::pedantic)]
+
 extern crate pancurses;
 use pancurses as curses;
 
@@ -35,43 +38,22 @@ impl CharCounts {
 }
 
 /// Writes the entire state of the window out to the screen.
-fn render(wnd: &curses::Window, text: &String, _counts: &CharCounts)
+fn render(wnd: &curses::Window, text: &str, counts: &CharCounts)
 {
-    // Shoot, I don't know how to use curses best!
-    let screen_width = wnd.get_max_x();
-
-    // first, print out 1st 5 lines of text
-    const MAX_ROWS: i32 = 5;
-    let str_n_rows
-        = ((text.len() as f32) / (screen_width as f32)).ceil() as i32;
-
-    // just try a little functional weirdness and hope I get this math right
-    let text_row_width = screen_width - 2;
-    let text_print_rows = (std::cmp::max(str_n_rows - MAX_ROWS, 0)..str_n_rows)
-        .map(|row| text.get(
-                (row * text_row_width) as usize
-                ..std::cmp::min(
-                    (row + 1) * text_row_width,
-                    text.len() as i32) as usize));
-
-    wnd.mv(0, 0);
-    for print_row_n in text_print_rows {
-        wnd.printw("| ");
-        wnd.printw(print_row_n.unwrap_or("HELP! BAD INDEX!!!"));
-    }
+    unimplemented!()
 }
 
 fn main_loop(window: &curses::Window) {
-    let mut counts: CharCounts = Default::default();
+    let mut char_counts = CharCounts::default();
     let mut text = String::new();
 
     loop {
-        render(&window, &text, &counts);
+        render(&window, &text, &char_counts);
 
         match window.getch() {
             Some(curses::Input::Character(input)) => {
                 text.push(input);
-                counts.add_char(input);
+                char_counts.add_char(input);
             },
             _ => break
         };
